75e7a57b948ba19f5e7a398160c8e30189b9e891
Cauã <Cau@192.168.1.7>
2016-03-13 17:50:00 -0300

    Atualizando Main Separando Send e Receive em threads separadas ok Adicionado painel de texto ok Adicionado classe criptografia Adicionado classe usuario passo 5

--- src/peer2peerproject/Peer2PeerProject.java
+++ src/peer2peerproject/Peer2PeerProject.java
@@ -2,27 +2,33 @@ package peer2peerproject;
 
 import java.net.*;
 import java.io.*;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class Peer2PeerProject {
 
-    public static void main(String[] args) throws UnknownHostException, IOException {
+    public static void main(String[] args) throws UnknownHostException, IOException, NoSuchAlgorithmException {
         Scanner sc = new Scanner(System.in);
         ConnectionSend cs;
         ConnectionReceive cr;
-
-        String ip = "224.224.224.224", userName;
+        Interface tela;
+        String ip = "224.224.224.224", userName = null;
         int port = 6789;
+        UserData user;
+        Criptografar cript = new Criptografar();
+        
+
         InetAddress group = InetAddress.getByName(ip);
         MulticastSocket ms = new MulticastSocket(port);
         ms.joinGroup(group);
         System.out.print("Digite o nome de usuario: ");
         userName = sc.nextLine();
+        user = new UserData(userName, group, cript.getPublicKey());
 
-        cs = new ConnectionSend(ms, userName, group, port);
+        cs = new ConnectionSend(ms, userName, group, port, cript);
         cs.start();
-        Interface tela = new Interface(cs);
-        cr = new ConnectionReceive(ms, userName, tela);
+        tela = new Interface(cs);
+        cr = new ConnectionReceive(ms, userName, tela, user, cript);
         cr.start();
         tela.setVisible(true);
 
